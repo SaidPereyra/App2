3706c550c691182029bd90a3c426e42d4b3895d5
SaidPereyra <zaidpereyra@gmail.com>
2021-09-22 17:36:04 -0500

    Agregar archivos de proyecto.

---
+++ App2/App2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App2.ViewModels
+{
+    class MainPageViewModel : ViewModelBase
+    {
+
+
+        private string _nombre;
+
+        public MainPageViewModel()
+        {
+            _nombre = "holi";
+        }
+
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (string.Equals(_nombre, value)) return;
+                _nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(SinUpdate));
+            }
+        }
+
+        public string SinUpdate
+        {
+            get => _nombre;
+        }
+
+    }
+}
+
